a293fae302562902d96fac66bf35a9b4392fe919
Luis Enrique Martínez Gómez <lumartineck@gmail.com>
2013-05-28 11:26:26 -0500

    Se modifica para version compresa

--- app/view/tablet/Main.js
+++ app/view/tablet/Main.js
@@ -27,18 +27,126 @@ Ext.define('SenChanvas.view.tablet.Main', {
         items: [
             {
                 xtype: 'panel',
-                id:'principalPanel',
+                id: 'principalPanel',
                 title: 'Fancy',
+                layout: 'vbox',
                 items: [
                     {
-                        xtype:'component',
-                        html: 'Pinch me',
-                        itemId:'redSquare',
-                        top: 10,
-                        left: 10,
-                        width: 300,
-                        height: 300,
-                        style: 'background: red'
+                        xtype: 'container',
+                        layout:'hbox',
+                        style: {
+                            'border' : 'solid 1px #D7D9DE',
+                            'border-radius' : '9px'
+                        },
+                        margin: '3 5 3 5',
+                        defaults: {
+                            iconMask: true,
+                            xtype: 'button',
+                            margin: '3 3 3 3',
+                            ui: 'action'
+                        },
+                        items: [
+                            {xtype: 'spacer'},
+                            {iconCls: 'refresh', action: 'rotateR'},
+                            {iconCls: 'refresh', action: 'rotateL'},
+                            {iconCls: 'arrow_up', action: 'toFront'},
+                            {iconCls: 'arrow_down', action: 'toBack'},
+                            {iconCls: 'delete', action: 'delete'}
+                        ]
+                    },
+                    {
+                        xtype: 'container',
+                        id: 'dropable',
+                        flex: 4,
+                        style: {
+                            'border' : 'solid 3px #3CA9D0',
+                            'border-radius' : '5px'
+                        },
+                        margin: '3 5 3 5'/*,
+                        items: [
+                            {
+                                xtype: 'component',
+                                html: 'Pinch me',
+                                itemId: 'redSquare',
+                                top: 10,
+                                left: 10,
+                                width: 300,
+                                height: 300,
+                                style: 'background: red'
+                            },
+                            {
+                                xtype: 'component',
+                                html: 'Pinch me',
+                                itemId: 'blueSquare',
+                                top: 10,
+                                left: 200,
+                                width: 300,
+                                height: 300,
+                                style: 'background: blue'}
+                        ]*/
+                    },
+                    {
+                        xtype: 'container',
+                        id: 'draggsCnt',
+                        layout: 'hbox',
+                        flex: 1,
+                        items: [{
+                            xtype: 'component',
+                            draggable: true,
+                            html: '<img src="./resources/images/001.jpg" width="50" height="50">',
+                            getSrcImage:function(){
+                                return './resources/images/001.jpg';
+                            }
+                        },{
+                            xtype: 'component',
+                            draggable: true,
+                            html: '<img src="./resources/images/002.jpg" width="50" height="50">',
+                            getSrcImage:function(){
+                                return './resources/images/002.jpg';
+                            }
+                        },{
+                            xtype: 'component',
+                            draggable: true,
+                            html: '<img src="./resources/images/003.jpg" width="50" height="50">',
+                            getSrcImage:function(){
+                                return './resources/images/003.jpg';
+                            }
+                        },{
+                            xtype: 'component',
+                            draggable: true,
+                            html: '<img src="./resources/images/004.jpg" width="50" height="50">',
+                            getSrcImage:function(){
+                                return './resources/images/004.jpg';
+                            }
+                        },{
+                            xtype: 'component',
+                            draggable: true,
+                            html: '<img src="./resources/images/005.jpg" width="50" height="50">',
+                            getSrcImage:function(){
+                                return './resources/images/005.jpg';
+                            }
+                        },{
+                            xtype: 'component',
+                            getSrcImage:function(){
+                                return './resources/images/001.jpg';
+                            },
+                            draggable: true,
+                            html: '<img src="./resources/images/001.jpg" width="50" height="50">'
+                        },{
+                            xtype: 'component',
+                            draggable: true,
+                            html: '<img src="./resources/images/002.jpg" width="50" height="50">',
+                            getSrcImage:function(){
+                                return './resources/images/002.jpg';
+                            }
+                        },{
+                            xtype: 'component',
+                            draggable: true,
+                            html: '<img src="./resources/images/003.jpg" width="50" height="50">',
+                            getSrcImage:function(){
+                                return './resources/images/003.jpg';
+                            }
+                        }]
                     }
                 ]
             }
